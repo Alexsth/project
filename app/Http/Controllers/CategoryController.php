e041a6f342ffbc6eeb7526b982eac9cd30a5abb9
AlexSth <alexshrestha77@gmail.com>
2020-10-07 12:59:17 +0545

    First add

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+Use Illuminate\Support\Str;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $productCategory = Category::all();
+        return view('backend.category.index',compact('productCategory'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $productCategory = Category::where('parent','0')->get();
+        return view('backend.category.create',compact('productCategory'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $slug= Str::slug($request->title, '-');
+        Category::create([
+            'title'=> $request->title,
+            'description'=>$request->description,
+            'parent'=>$request->parent,
+            'meta_title'=>$request->meta_title,
+            'meta_description'=>$request->meta_description,
+            'meta_keywords'=>$request->meta_keywords,
+            'slug'=> $slug
+        ]);
+        return redirect()->route('categories.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return view('backend.category.show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        $productCategory = Category::where('parent','0')->get();
+        return view('backend.category.update',compact('category','productCategory'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $slug= Str::slug('$request->title', '-');
+        $category->update([
+            'title'=> $request->title,
+            'description'=>$request->description,
+            'parent'=>$request->parent,
+            'meta_title=>$request->meta_title',
+            'meta_description'=>$request->meta_description,
+            'meta_keywords'=>$request->meta_keywords,
+            'slug'=> $slug
+        ]);
+        return redirect()->route('categories.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return redirect()-> route('categories.index');
+    }
+}
---
+++ database/migrations/2020_10_06_063359_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description');
+            $table->unsignedBigInteger('parent')->default(0);
+            $table->string('meta_title');
+            $table->text('meta_description');
+            $table->string('meta_keywords');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
